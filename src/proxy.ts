c7718b16d339723fb832a9656c1afd0d8b5f42f6
Aleksandr <alex.mobicraft@gmail.com>
2026-03-14 18:55:30 +0100

    feat: add proxy implementation in `proxy.ts`.

--- src/proxy.ts
+++ src/proxy.ts
@@ -14,6 +14,6 @@ export const config = {
      * - favicon.ico
      * - публичных ассетов (изображения, шрифты и т.д.)
      */
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|auth/confirm|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
